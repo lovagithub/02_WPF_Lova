b7cccf9fc7eeef878ca74976c1ad67ab0ef510eb
Lova Juhlin <lova.juhlin@outlook.com>
2023-02-05 13:03:54 +0100

    2023-02-05

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _02_WPF
+namespace _02_WPF_Lova
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
